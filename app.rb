f763fb77a5049abef51528fb55167d4daabb86f4
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-02 20:42:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/:p' do
-    params.values.to_upcase
+    params[:p].to_upcase
   end
 
   get '/reversename/:name' do |name|
